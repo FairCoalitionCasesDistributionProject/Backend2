805770e67a9d66caaac906bc89fe2b354d91a79a
Andrey <andreyba2000@gmail.com>
2022-04-13 16:33:34 +0300

    added tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class AccountTests(APITestCase):
+    def test_send_json_request1(self):
+        data = {'key': '1.1.1.1.1.1.1', 'items': 3, 'mandates': [1, 2, 3], 'preferences': [[1, 2, 3], [2, 1, 3], [3, 2, 1]]}
+        response = self.client.post("https://faircol.herokuapp.com/api/", data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(response.data, -1)
+        
+    def test_send_wrong_json_request(self):
+        data = {'key': '1.1.1.1.1.1.1', 'items': 3, 'mandates': [1, 2, 3], 'preferences': [[2, 1, 3], [3, 2, 1]]}
+        response = self.client.post("https://faircol.herokuapp.com/api/", data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, -1)
+
+    def test_send_not_json_request(self):
+        data = "not json"
+        response = self.client.post("https://faircol.herokuapp.com/api/", data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, -1)
+
+    def test_retriving_session(self):
+        data = {'key': '3.3.3.3.3.3.3', 'items': 3, 'mandates': [1, 2, 3], 'preferences': [[1, 2, 3], [2, 1, 3], [3, 2, 1]]}
+        respone = self.client.post("https://faircol.herokuapp.com/api/", data, format='json')
+        self.assertEqual(respone.status_code, status.HTTP_200_OK)
+        data = {'key': '3.3.3.3.3.3.3'}
+        respone = self.client.post("https://faircol.herokuapp.com/api/getsave", data, format='json')
+        self.assertEqual(respone.status_code, status.HTTP_200_OK)
+        self.assertEqual(respone.data, "[[1, 2, 3], [2, 1, 3], [3, 2, 1]]")
+        
+    def test_retriving_nonexisting_session(self):
+        data = {'key': 'Not.a.key.at.all'}
+        respone = self.client.post("https://faircol.herokuapp.com/api/getsave", data, format='json')
+        self.assertEqual(respone.status_code, status.HTTP_200_OK)
+        self.assertEqual(respone.data, -1)
+    
+    def test_retriving_not_a_json_key_session(self):
+        data = "not json"
+        respone = self.client.post("https://faircol.herokuapp.com/api/getsave", data, format='json')
+        self.assertEqual(respone.status_code, status.HTTP_200_OK)
+        self.assertEqual(respone.data, -1)
+        
